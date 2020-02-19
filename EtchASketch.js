5b9c18a345f9ff2bd4b435c379dd0ff41b282a99
GregoryPrograms <greg.dost@yahoo.com>
2020-02-19 10:53:15 -0800

    Moved grid creation to a function, began reformatting code to allow for changing of grid dimensions

--- EtchASketch.js
+++ EtchASketch.js
@@ -3,23 +3,37 @@ const sketchboard = document.createElement('div');
 sketchboard.style.height="576px";
 sketchboard.style.width="576px";
 sketchboard.style.border = "solid #0000FF";
-var sketchArray = [];
+createGrid();
+document.body.appendChild(sketchboard);
 
 //Creates all elements on sketchboard, assigns them 'grid' class.
-for(var gridColIndex = 0; gridColIndex < GRID_DIM; gridColIndex++){
-    var sketchCol = document.createElement('div');
-    sketchCol.style.cssFloat = "left";
-    for(var gridRowIndex = 0; gridRowIndex < GRID_DIM; gridRowIndex++){
-        var gridBlock = document.createElement('div');
-        gridBlock.style.height="36px";
-        gridBlock.style.width="36px";
-        gridBlock.addEventListener("mouseover", function (e) {
-            e.target.style.background = 'blue';
-          });
-        //gridBlock.style.border = "solid #0000FF";
-        sketchArray.push(gridBlock);  
-        sketchCol.appendChild(gridBlock);
+function createGrid(){
+    for(var gridColIndex = 0; gridColIndex < GRID_DIM; gridColIndex++){
+        var sketchCol = document.createElement('div');
+        sketchCol.style.cssFloat = "left";
+        for(var gridRowIndex = 0; gridRowIndex < GRID_DIM; gridRowIndex++){
+            var gridBlock = document.createElement('div');
+            gridBlock.style.height=(576 / GRID_DIM) + "px";
+            gridBlock.style.width=(576 / GRID_DIM) + "px";
+            gridBlock.addEventListener("mouseover", function (e) {
+                e.target.style.background = 'blue';
+            });  
+            sketchCol.appendChild(gridBlock);
+        }
+        sketchboard.appendChild(sketchCol);
     }
-    sketchboard.appendChild(sketchCol);
 }
-document.body.appendChild(sketchboard);
+
+
+//Buttons assigned to actions related to the sketchboard.
+var clearBoard = document.createElement('button');
+clearBoard.innerHTML = "Clear Board";
+//On clicking the 'clear board' button, iterates through an array of the grids, setting all of their backgrounds to white.
+clearBoard.onclick = function(){
+    //sketchArray.forEach(function(gridBlock, index){
+    //    gridBlock.style.background = 'white';
+    //})
+    sketchboard.textContent = '';
+    createGrid();
+}
+document.body.appendChild(clearBoard);
